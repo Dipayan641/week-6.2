fae6ac27f351f795e502eb72263b784f8d8e17a1
Dipayan641 <mitradipayan641@gmail.com>
2025-07-01 19:39:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import App1 from './app1.jsx'
-import App2 from './app2.jsx'
+// import App1 from './app1.jsx'
+// import App2 from './app2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
