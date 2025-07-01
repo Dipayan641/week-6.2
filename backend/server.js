9099df6cb9552a5f05e6ffceabe7b3c9c05caaec
Dipayan641 <mitradipayan641@gmail.com>
2025-07-01 20:48:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const port =3000;
+const port =3001;
 
 
 // Middleware to handle CORS and parse incoming JSON bodies
